544c4d80f0b349b08e6c81109f584c87f671c761
Ares Jilani <ares.jilani@elev.ga.ntig>
2024-08-30 13:57:05 +0200

    första

---
+++ looktext/ConsoleApp1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Net.Http.Headers;
+
+string Adjective1;
+
+Console.WriteLine("write adjective");
+Adjective1 = Console.ReadLine();
+
+string Namefriend;
+
+Console.WriteLine("name");
+Namefriend = Console.ReadLine();
+
+string Adjective2;
+
+Console.WriteLine("write adjective");
+Adjective2 = Console.ReadLine();
+
+string noun1;
+
+Console.WriteLine("write noun");
+noun1 = Console.ReadLine();
+
+string Adjective3;
+
+Console.WriteLine("write adjective");
+Adjective3 = Console.ReadLine();
+
+string Adjective4;
+
+Console.WriteLine("write adjective");
+Adjective4 = Console.ReadLine();
+
+string animal;
+
+Console.WriteLine("write animal");
+animal = Console.ReadLine();
+
+string TypeOfFood;
+
+Console.WriteLine("write a type of food");
+TypeOfFood = Console.ReadLine();
+
+string Beverage;
+
+Console.WriteLine("write beverage");
+Beverage = Console.ReadLine();
+
+string Adjective5;
+
+Console.WriteLine("write adjective");
+Adjective5 = Console.ReadLine();
+
+string noun2;
+
+Console.WriteLine("write noun");
+noun2 = Console.ReadLine();
+
+string Adjective6;
+
+Console.WriteLine("write adjective");
+Adjective6 = Console.ReadLine();
+
+string color;
+
+Console.WriteLine("write color");
+color = Console.ReadLine();
+
+string number;
+
+Console.WriteLine("write a number");
+number = Console.ReadLine();
+
+string PluralNoun;
+
+Console.WriteLine("write a plural noun");
+PluralNoun = Console.ReadLine();
+
+string Adjective7;
+
+Console.WriteLine("write adjective");
+Adjective7 = Console.ReadLine();
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+// Console.WriteLine("Wagwan " + name + "!");
+Console.WriteLine($"Today, I went to the beach with my {Adjective1} friend, {Namefriend}. We decided to build a {Adjective2} sandcastle in the shape of a {noun1}. After that, we went for a swim in the {Adjective3} ocean, where we saw a {Adjective4} {animal} swimming by. For lunch we had {TypeOfFood} and drank {Beverage} while sitting under a {Adjective5} {noun2}. The sun was {Adjective6}, and the sky was {color}, making it the perfect day. Before we left, we collected {number} {PluralNoun} as souvenirs. It was a/an {Adjective7} day at the beach!");
+
+Console.ReadLine();
+
